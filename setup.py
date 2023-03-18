3c6755974dddeed1799c00799e901af3ab5ee96f
anandgupta42 <anand@altimate.ai>
2023-03-18 15:59:31 -0700

    bumping version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="altimate_django",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=["Django>=2.0", "tabulate>=0.9.0", "termcolor>=1.1.0"],
     author="Altimate Inc.",
